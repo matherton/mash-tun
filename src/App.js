0c0df9b5fb86da792f84f228534850459197d942
Mark Atherton <mark@insightinternet.co.uk>
2024-11-21 14:06:15 +0000

    added media query to Header.css to make it respond better

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import MenuList from "./components/MenuList";
-import logo from "./assets/images/logo.jpg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <hr />
+      <h1>MENU</h1>
       <MenuList key={"burgers"} />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
 import React from "react";
 import logo from "../assets/images/logo.jpg";
+import Insta from "../assets/images/black_instagram-logo.png";
+
+import "./Header.css";
 
 export default function Header() {
   return (
-    <>
-      <img src={logo} alt="logo" className="logo" />
-      <hr />
-      <h1>MENU</h1>
-    </>
+    <div className="header">
+      <div className="spacer"></div>
+      <div>
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/mashtunedinburgh/"
+          alt="links to the Mash Tun Instagram account in new window"
+        >
+          <img
+            src={Insta}
+            alt="instagram icon"
+            title="Links to Mash Tun Instagram"
+            className="instagram"
+          />
+        </a>
+      </div>
+    </div>
   );
 }
